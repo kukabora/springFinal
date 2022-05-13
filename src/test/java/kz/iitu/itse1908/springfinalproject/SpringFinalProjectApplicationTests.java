dece5fbf43276a29762feec82289658429593ecc
kukabora <youdoit@mail.ru>
2022-05-13 15:56:57 +0600

    Initial commit

---
+++ src/test/java/kz/iitu/itse1908/springfinalproject/SpringFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.itse1908.springfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
