08ea0da090bc09186fb482ce9b595489d662637e
kukabora <youdoit@mail.ru>
2022-05-13 18:59:12 +0600

    Added configuration file for datasource manual configuration.

---
+++ src/main/java/kz/iitu/itse1908/springfinalproject/Configuration/DatasourceConfiguration.java
@@ -0,0 +1,72 @@
+package kz.iitu.itse1908.springfinalproject.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.*;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+@Configuration
+@EnableScheduling
+@EnableAspectJAutoProxy
+@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
+public class DatasourceConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        Properties props = new Properties();
+
+        props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+        props.setProperty("dataSource.user", "superuser");
+        props.setProperty("dataSource.password", "46452020");
+        props.setProperty("dataSource.databaseName", "variant2_lee");
+        props.put("dataSource.logWriter", new PrintWriter(System.out));
+
+        HikariConfig config = new HikariConfig(props);
+        HikariDataSource dataSource = new HikariDataSource(config);
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProp = new Properties();
+        hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        hibernateProp.put("hibernate.format_sql", true);
+        hibernateProp.put("hibernate.use_sql_comments", true);
+        hibernateProp.put("hibernate.show_sql", true);
+        hibernateProp.put("org.hibernate.envers.audit_table_suffix", "_AUDIT_LOG");
+        hibernateProp.put("hibernate.max_fetch_depth", 3);
+        hibernateProp.put("hibernate.jdbc.batch_size", 10);
+        hibernateProp.put("hibernate.jdbc.fetch_size", 50);
+        hibernateProp.put("spring.jpa.properties.hibernate.enable_lazy_load_no_trans", true);
+
+        return hibernateProp;
+    }
+
+    @Bean(name="entityManagerFactory")
+    public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("kz.iitu.itse1908.springfinalproject");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws IOException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+
+
+}
--- src/main/java/kz/iitu/itse1908/springfinalproject/SpringFinalProjectApplication.java
+++ src/main/java/kz/iitu/itse1908/springfinalproject/SpringFinalProjectApplication.java
@@ -2,8 +2,19 @@ package kz.iitu.itse1908.springfinalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@Component
+@EntityScan("kz.iitu.itse1908.springfinalproject.*")
+@EnableAspectJAutoProxy
+@EnableWebMvc
+@EnableScheduling
 public class SpringFinalProjectApplication {
 
     public static void main(String[] args) {
